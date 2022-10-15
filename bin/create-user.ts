bd7ffc9c11fbf6648d82f8b99d70c669e4fd151a
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-10-15 09:58:23 +0900

    configure: prisma

---
+++ bin/create-user.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: process.argv[1] ?? "Alice",
+      email: process.argv[2] ?? "alice@prisma.io",
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
