e774c0d9143284a5ee00c087fe7c6a2e41f9ac76
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-10-15 17:35:52 +0900

    add: Dockerfile

--- server/trpc.ts
+++ server/trpc.ts
@@ -7,16 +7,17 @@ export const router = t.router;
 export const publicProcedure = t.procedure;
 
 const isAuthed = t.middleware(({ next, ctx }) => {
-  if (!ctx.session?.user?.email) {
-    throw new TRPCError({
-      code: "UNAUTHORIZED",
-    });
-  }
-  return next({
-    ctx: {
-      session: ctx.session,
-    },
-  });
+  // if (!ctx.session?.user?.email) {
+  //   throw new TRPCError({
+  //     code: "UNAUTHORIZED",
+  //   });
+  // }
+  // return next({
+  //   ctx: {
+  //     session: ctx.session,
+  //   },
+  // });
+  return next();
 });
 
 export const protectedProcedure = t.procedure.use(isAuthed);
