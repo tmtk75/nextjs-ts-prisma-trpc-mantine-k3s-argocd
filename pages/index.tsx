e676f79954db560d681a2c81dec8120df0b348f4
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-10-15 10:31:26 +0900

    configure: tRPC

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { Post, PrismaClient, User } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import type { NextPage } from "next";
+import { trpc } from "../utils/trpc";
 
 interface Props {
   users: User[];
@@ -7,22 +8,27 @@ interface Props {
 
 const Home: NextPage<Props> = (props: Props) => {
   const { users } = props;
+  const hello = trpc.hello.useQuery({ text: "Alice" });
+  if (!hello.data) {
+    return <>Loading...</>;
+  }
   return (
     <>
-      <div>posts.length: {users.length}</div>
-      {users.map((p) => (
+      <div>posts.length: {users?.length}</div>
+      {users?.map((p) => (
         <div key={p.id}>id: {p.id}</div>
       ))}
       <div>
         <button onClick={async () => {}}>create a new user</button>
       </div>
+      <div>{hello.data.greeting}</div>
     </>
   );
 };
 
 export default Home;
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const prisma = new PrismaClient();
   const users = await prisma.user.findMany();
   return {
---
+++ server/trpc.ts
@@ -0,0 +1,22 @@
+import { initTRPC, TRPCError } from "@trpc/server";
+import superjson from "superjson";
+
+const t = initTRPC.create({ transformer: superjson });
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.session?.user?.email) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    });
+  }
+  return next({
+    ctx: {
+      session: ctx.session,
+    },
+  });
+});
+
+export const protectedProcedure = t.procedure.use(isAuthed);
